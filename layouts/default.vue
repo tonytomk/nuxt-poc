bfc6883c7e6dd0a923bc3d43a28c4e1a20f68c90
Tony Tom K <tony.tomk@thermofisher.com>
2019-03-26 13:00:58 +0530

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <nav-bar />
+    <nuxt />
+  </div>
+</template>
+<script>
+import NavBar from '~/components/NavBar.vue'
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+html {
+  -webkit-text-size-adjust: 100%;
+}
+body {
+  margin: 0;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 16px;
+  line-height: 1.15;
+}
+h1 {
+  font-size: 50px;
+  font-weight: 700px;
+}
+</style>
---
+++ pages/create.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>
+           Create an Events
+        </h1>
+    </div>
+</template>
